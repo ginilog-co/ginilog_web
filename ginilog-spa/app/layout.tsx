1a4d62ff268f16bc66ff8956a53d3f7e73c42bc9
Georgina Kimani <njokikimani001@gmail.com>
2026-04-19 11:25:29 +0300

    added analytics

--- ginilog-spa/app/layout.tsx
+++ ginilog-spa/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AppShell>{children}</AppShell>
+        <Analytics />
       </body>
     </html>
   );
