29ef51db155e2066770f9c312e42e76d7a5d2b4b
Francis_samuel005 <82176840+francissame@users.noreply.github.com>
2026-06-20 22:19:17 +0000

    menu added on mobile

--- ginilog-spa/.next/static/chunks/app/landing-page/page-a74bd9e3c8793f5e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[446],{1966:(e,s,_)=>{Promise.resolve().then(_.bind(_,832))}},e=>{e.O(0,[297,531,20,832,441,255,358],()=>e(e.s=1966)),_N_E=e.O()}]);
