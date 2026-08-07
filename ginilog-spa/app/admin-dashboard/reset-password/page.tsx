1e6108be629325dee665a533bc8fd49f8a661c37
Francis_samuel005 <82176840+francissame@users.noreply.github.com>
2026-08-07 14:26:45 +0000

    new

--- ginilog-spa/app/admin-dashboard/reset-password/page.tsx
+++ ginilog-spa/app/admin-dashboard/reset-password/page.tsx
@@ -132,7 +132,7 @@ function ResetPasswordForm() {
 
             <div>
               <Label htmlFor="confirmPassword" className="text-gray-700">
-                Confirm Password
+                Confirm Password.
               </Label>
               <div className="relative mt-1">
                 <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
